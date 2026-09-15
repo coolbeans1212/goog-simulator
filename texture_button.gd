155a9cc1894b91222844289d899df00d86737a60
Matei sHomePage <matei@mateishome.page>
2026-09-15 23:38:57 +0100

    gsfjifsdjisdfij

---
+++ texture_button.gd
@@ -0,0 +1,23 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	print("goog")
+
+
+func _on_button_down() -> void:
+	self.flip_v = true
+	
+
+func _on_button_up() -> void:
+	self.flip_v = false
